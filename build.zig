4ecfc91cab0e30273a561745f582205f01754781
Ben Jordan <ben@poecoh.com>
2024-05-07 23:34:32 -0400

    stupid simple again

--- build.zig
+++ build.zig
@@ -3,18 +3,10 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const module = b.addModule("SimpleLib", .{
+    _ = b.addModule("SimpleLib", .{
         .root_source_file = .{ .path = "src/root.zig" },
         .target = target,
         .optimize = optimize,
     });
-    const unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/test.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-    unit_tests.root_module.addImport("SimpleLib", module);
-    const run_unit_tests = b.addRunArtifact(unit_tests);
-    const test_step = b.step("test", "Run the tests");
-    test_step.dependOn(&run_unit_tests.step);
+
 }
--- src/test.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-const SimpleLib = @import("SimpleLib");
-
-
-test "SimpleLib" {
-    const sl = SimpleLib{};
-    std.testing.expectEqualStrings("Hello, World!", sl.getStuff());
-}
