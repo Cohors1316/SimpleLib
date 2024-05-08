8107c2eb184cc10723596945462d93c3afbae3a3
Ben Jordan <ben@poecoh.com>
2024-05-08 07:57:12 -0400

    tweaked for sanity

--- build.zig
+++ build.zig
@@ -3,10 +3,9 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    _ = b.addModule("SimpleLib", .{
-        .root_source_file = .{ .path = "src/root.zig" },
+    _ = b.addModule("SimpleFile", .{
+        .root_source_file = .{ .path = "src/SimpleFile.zig" },
         .target = target,
         .optimize = optimize,
     });
-
 }
